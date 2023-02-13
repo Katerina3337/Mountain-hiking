8189403568cf150878f90e621b5170ee105361cf
Katerina3337 <katerina_samkova@mail.ru>
2023-02-13 22:25:45 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <mountain-file>
+
+  </mountain-file>
+</template>
+
+<script setup>
+import MountainFile from "@/components/MountainFile.vue";
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/MountainFile.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="header">
+    <nav class="nav-wrapper">
+      <div class="container-wide">
+        <div class="row space-between">
+          <div class="logo">MNTN</div>
+          <div class="nav">
+            <ul>
+              <li><a href="#">Equipment</a></li>
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Blog</a></li>
+            </ul>
+          </div>
+          <a href="#" class="account">Account</a>
+        </div>
+      </div>
+    </nav>
+
+    <a href="#" class="hero">
+      <div class="hero-category">A Hiking guide</div>
+      <h1 class="hero-title">Be prepared for the Mountains and beyond!</h1>
+      <p class="hero-scroll">scroll down</p>
+    </a>
+  </header>
+</template>
+
+<style scoped></style>
